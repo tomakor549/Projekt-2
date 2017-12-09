a894971baab9ae6cfec8590628b45bc16a603d4c
Tenebra Venator <tomakor549@student.polsl.pl>
2017-12-09 21:08:07 +0100

    Wpisywanie przez użytkownika

--- Projekt-2/Projekt-2/Functions.cpp
+++ Projekt-2/Projekt-2/Functions.cpp
@@ -1,7 +1,14 @@
 #include "Functions.h"
 #include "stdafx.h"
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <string>
 
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
 void add_book()
 {
 
@@ -16,5 +23,31 @@ int reachability()
 int amount()
 {
 
+	return 0;
+}
+
+int wpisz()
+{
+	string date;
+	int choise;
+	getline(cin, date);
+	int length = date.length();
+
+	if (length != 1)
+	{
+		return 0;
+	}
+	else
+	{
+		if (date[0] == '1' || date[0] == '2' || date[0] == '3' || date[0] == '4' || date[0] == '5')
+		{
+			choise = atoi(date.c_str());
+			return choise;
+		}
+		else
+		{
+			return 0;
+		}
+	}
 	return 0;
 }
--- Projekt-2/Projekt-2/Functions.h
+++ Projekt-2/Projekt-2/Functions.h
@@ -2,4 +2,11 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <string>
 
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
